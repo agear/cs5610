7403f96c7d4f216e97308382eb614dbebe203ad9
Alex Gear <alexgear@gmail.com>
2020-06-02 20:02:57 -0400

    The List API

--- server/server.js
+++ server/server.js
@@ -4,18 +4,23 @@ const { ApolloServer } = require('apollo-server-express');
 
 let aboutMessage = "Issue Tracker API v1.0";
 
-// const typeDefs = `
-//     type Query {
-//         about: String!
-//     }
-//     type Mutation {
-//         setAboutMessage(message: String!): String
-//     }
-// `;
+const issueDB = [
+  {
+    id: 1, status: 'New', owner: 'Ravan', effort: 5,
+    created: new Date('2019-01-15'), due: undefined,
+    title: 'Error in console when clicking Add',
+  }, 
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+    created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+    title: 'Missing bottom border on panel',
+  },
+];
 
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
     setAboutMessage,
@@ -24,6 +29,10 @@ const resolvers = {
 
 function setAboutMessage(_, { message }) {
   return aboutMessage = message;
+}
+
+function issueList() {
+  return issueDB;
 }
 
 const server = new ApolloServer({
