d1acd8129acac6fa50cd264a87c3b5f225dacf6f
Alex Gear <alexgear@gmail.com>
2020-06-28 13:14:41 -0400

    MongoDB Node.js Driver

---
+++ scripts/trymongo.js
@@ -0,0 +1,73 @@
+const url = 'mongodb://localhost:27017/issuetracker';
+const { MongoClient } = require('mongodb');
+
+
+// Atlas URL - replace UUU with user, PPP with password, XXX with hostname
+// const url = 'mongodb+srv://UUU:PPP@clusterO-XXX.mongodb.net/issuetracker?retryWrites=true';
+
+// mLab URL - replace UUU with user, PPP with password, XXX with hostname
+// const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
+
+function testWithCallbacks(callback) {
+    console.log('\n--- testWithCallbacks ---');
+    const client = new MongoClient(url, { useNewUrlParser: true });
+
+    client.connect(function(err, client) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        console.log('Connected to MongoDB');
+
+        const db = client.db();
+        const collection = db.collection('employees');
+        const employee = { id: 1.0, name: 'A. Callback', age: 55 };
+        collection.insertOne(employee, function(err, result) {
+            if (err) {
+                client.close();
+                callback(err);
+                return;
+            }
+            console.log('Result of insert:\n', result.insertedId);
+
+            collection.find({_id: result.insertedId})
+            .toArray(function(err, docs) {
+                    if (err) {
+                        client.close();
+                        callback(err);
+                        return;
+                    }
+                    console.log('Result of find:\n', docs);
+                    client.close();
+                    callback(err);
+            });
+        });
+    });
+}
+
+// const client = new MongoClient(url, { useNewUrlParser: true });
+// client.connect();
+
+// client.connect(function(err, client) {
+//     const db = client.db();
+// }
+
+// const collection = db.collection('employees');
+
+// const employee = { id: 1, name: 'A. Callback', age: 23 };
+// collection.insertOne(employee, function(err, result) {
+//     console.log("Result of insert:\n", result.insertedId);
+// })
+
+// collection.find({ _id: result.insertedId})
+//     .toArray(function(err, docs)) {
+//         console.log('Result of find:\n', docs);
+//     }
+
+// client.close();
+
+testWithCallbacks(function(err) {
+    if (err) {
+        console.log(err);
+    }
+})
